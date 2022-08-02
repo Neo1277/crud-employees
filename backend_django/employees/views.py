60c5084cddca9dde79713d190126f8e472b13206
Marlon Meneses <marlon-armando@hotmail.com>
2022-08-02 14:51:55 -0500

    Simple pagination added to RetrieveEmployeesView

--- backend_django/employees/views.py
+++ backend_django/employees/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics
+from rest_framework import generics, pagination
 
 from .models import Employees
 
@@ -7,6 +7,13 @@ from .serializers import EmployeesSerializer
 # Open Django console
 # python manage.py shell
 
+# pagination: https://www.django-rest-framework.org/api-guide/pagination/
+class LargeResultsSetPagination(pagination.PageNumberPagination):
+    page_size = 10
+    page_size_query_param = 'page_size'
+    max_page_size = 10000
+
 class RetrieveEmployeesView(generics.ListAPIView):
     queryset = Employees.objects.get_all()
     serializer_class = EmployeesSerializer
+    pagination_class = LargeResultsSetPagination
