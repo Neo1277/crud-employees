a54e92391cb6f9458647eec5e34613e5dcdb014e
Marlon Meneses <marlon-armando@hotmail.com>
2022-08-05 12:03:36 -0500

    Filter search working in Employees paginated endpoint

--- backend_django/employees/views.py
+++ backend_django/employees/views.py
@@ -5,8 +5,7 @@ from .models import Employees, ThirdParties
 from .serializers import (
     RetrieveEmployeesSerializer,
     SaveThirdPartiesSerializer,
-    UpdateThirdPartiesSerializer,
-    RetrieveNewEmailSerializer
+    UpdateThirdPartiesSerializer
 )
 
 from django.http.response import JsonResponse
@@ -14,6 +13,8 @@ from django.http.response import JsonResponse
 from rest_framework.views import APIView
 
 from .email_generation import check_email
+
+from rest_framework import filters
 
 # Open Django console
 # python manage.py shell
@@ -24,10 +25,24 @@ class LargeResultsSetPagination(pagination.PageNumberPagination):
     page_size_query_param = 'page_size'
     max_page_size = 10000
 
+# https://stackoverflow.com/a/58375919
+# https://www.django-rest-framework.org/api-guide/filtering/#searchfilter
 class RetrieveEmployeesView(generics.ListAPIView):
     queryset = Employees.objects.get_all()
     serializer_class = RetrieveEmployeesSerializer
     pagination_class = LargeResultsSetPagination
+    filter_backends = [filters.SearchFilter]
+    search_fields = [
+        'third_party__identity_document',
+        'third_party__types_of_identity_documents__description',
+        'third_party__last_name',
+        'third_party__second_surname',
+        'third_party__first_name',
+        'third_party__middle_names',
+        'country',
+        'third_party__email',
+        'area__description',
+    ]
 
 # Generic views:
 # https://www.django-rest-framework.org/api-guide/generic-views/
