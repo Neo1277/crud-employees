3eb9c567c789d4149ec1ef9ae34e5a8d9a935fad
Marlon Meneses <marlon-armando@hotmail.com>
2022-08-08 07:28:15 -0500

    Reverse, cls, assertEqual and self.client implemented in tests

--- backend_django/employees/tests/test_views.py
+++ backend_django/employees/tests/test_views.py
@@ -1,5 +1,5 @@
-from django.urls import include, path, reverse
-from rest_framework.test import APITestCase, URLPatternsTestCase, APIClient
+from django.urls import reverse
+from rest_framework.test import APITestCase, APIClient
 from rest_framework import status
 from areas.models import Areas
 from types_of_identity_documents.models import TypesOfIdentityDocuments
@@ -8,23 +8,24 @@ from employees.models import ThirdParties, Employees
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
 
+
 class RetrieveEmployeesViewTest(APITestCase):
 
     @classmethod
     def setUpTestData(cls):
 
-        area = Areas.objects.create(
+        cls.first_area = Areas.objects.create(
             description="Administration"
         )
 
-        types_of_identity_documents = TypesOfIdentityDocuments.objects.create(
+        cls.first_types_of_identity_documents = TypesOfIdentityDocuments.objects.create(
             code="CC",
             description='Citizenship Card'
         )
 
-        third_party = ThirdParties.objects.create(
+        cls.first_third_party = ThirdParties.objects.create(
             identity_document="99999887",
-            types_of_identity_documents=types_of_identity_documents,
+            types_of_identity_documents=cls.first_types_of_identity_documents,
             last_name="MONTOYA",
             second_surname='PEREZ',
             first_name='JUAN',
@@ -32,32 +33,32 @@ class RetrieveEmployeesViewTest(APITestCase):
             email='juan.montoya@cidenet.com.co'
         )
 
-        employee = Employees.objects.create(
-            third_party=third_party,
+        cls.first_employee = Employees.objects.create(
+            third_party=cls.first_third_party,
             country='CO',
-            area=area,
+            area=cls.first_area,
             date_of_entry='2022-08-06 00:00:00'
         )
 
     def test_get_endpoint(self):
-        client = APIClient()
-
-        response = client.get('/api/employees')
-        assert response.status_code == 200
+        response = self.client.get(reverse('retrieve-employeesview'))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_get_endpoint_with_search_that_exists(self):
-        client = APIClient()
-
-        response = client.get('/api/employees?search=perez')
-        assert response.status_code == 200
+        response = self.client.get(reverse('retrieve-employeesview'), {'search': 'perez'})
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_get_endpoint_with_search_that_does_not_exists(self):
-        client = APIClient()
-
-        response = client.get('/api/employees?search=ricardo')
-        assert response.status_code == 200
+        response = self.client.get(reverse('retrieve-employeesview'), {'search': 'ricardo'})
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
 
 class CreateEmployeesViewTest(RetrieveEmployeesViewTest):
+
+    """
+    @classmethod
+    def setUpTestData(cls):
+        super(CreateEmployeesViewTest, cls).setUpTestData()
+    """
 
     def test_post_endpoint(self):
 
@@ -69,68 +70,65 @@ class CreateEmployeesViewTest(RetrieveEmployeesViewTest):
             'first_name': 'PAUL',
             'middle_names': 'DE LAS MONTANAS',
             'email': 'paul.jimenez@cidenet.com.us',
-            'types_of_identity_documents_id': 1,
+            'types_of_identity_documents_id': self.first_types_of_identity_documents.pk,
             'third_parties_employees':
                 {
                     'country': 'US',
                     'date_of_entry': current_date,
-                    'area_id': 1
+                    'area_id': self.first_area.pk
                 }
         }
 
-        client = APIClient()
-        response = client.post('/api/save-employees', data, format='json')
+        response = self.client.post(reverse('save-employeesview'), data, format='json')
 
-        assert response.status_code == 201
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
     def test_post_endpoint_send_repeated_data(self):
 
         current_date = datetime.now()
-        data = {
+        invalid_data = {
             'identity_document': '99999887',
             'last_name': 'MONTOYA',
             'second_surname': 'RAMIREZ',
             'first_name': 'JUAN',
             'middle_names': 'RUBEN',
-            'email': 'juan.montoyya@cidenet.com.us',
-            'types_of_identity_documents_id': 1,
+            'email': 'juan.montoya@cidenet.com.us',
+            'types_of_identity_documents_id': self.first_types_of_identity_documents.pk,
             'third_parties_employees':
                 {
                     'country': 'US',
                     'date_of_entry': current_date,
-                    'area_id': 1
+                    'area_id': self.first_area.pk
                 }
         }
 
-        client = APIClient()
-        response = client.post('/api/save-employees', data, format='json')
+        response = self.client.post(reverse('save-employeesview'), invalid_data, format='json')
 
-        assert response.status_code == 400
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
 
     def test_post_endpoint_send_invalid_data(self):
 
         current_date = datetime.now()
-        data = {
+        invalid_data = {
             'identity_document': '99999884977',
             'last_name': 'cárdona',
             'second_surname': 'RAMIREZ',
             'first_name': 'Luis',
             'middle_names': 'RUBEN',
             'email': 'luis.cardona@cidenet.com.us',
-            'types_of_identity_documents_id': 1,
+            'types_of_identity_documents_id': self.first_types_of_identity_documents.pk,
             'third_parties_employees':
                 {
                     'country': 'US',
                     'date_of_entry': current_date,
-                    'area_id': 1
+                    'area_id': self.first_area.pk
                 }
         }
 
-        client = APIClient()
-        response = client.post('/api/save-employees', data, format='json')
+        response = self.client.post(reverse('save-employeesview'), invalid_data, format='json')
 
-        assert response.status_code == 400
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_post_endpoint_date_after_valida_range(self):
 
@@ -142,19 +140,18 @@ class CreateEmployeesViewTest(RetrieveEmployeesViewTest):
             'first_name': 'CARLOS',
             'middle_names': 'ALBERTO',
             'email': 'carlos.garcia@cidenet.com.co',
-            'types_of_identity_documents_id': 1,
+            'types_of_identity_documents_id': self.first_types_of_identity_documents.pk,
             'third_parties_employees':
                 {
                     'country': 'CO',
                     'date_of_entry': date_after_valida_range,
-                    'area_id': 1
+                    'area_id': self.first_area.pk
                 }
         }
 
-        client = APIClient()
-        response = client.post('/api/save-employees', data, format='json')
+        response = self.client.post(reverse('save-employeesview'), data, format='json')
 
-        assert response.status_code == 400
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_post_endpoint_date_before_valida_range(self):
 
@@ -166,16 +163,15 @@ class CreateEmployeesViewTest(RetrieveEmployeesViewTest):
             'first_name': 'CARLOS',
             'middle_names': 'ALBERTO',
             'email': 'carlos.garcia@cidenet.com.co',
-            'types_of_identity_documents_id': 1,
+            'types_of_identity_documents_id': self.first_types_of_identity_documents.pk,
             'third_parties_employees':
                 {
                     'country': 'CO',
                     'date_of_entry': date_before_valida_range,
-                    'area_id': 1
+                    'area_id': self.first_area.pk
                 }
         }
 
-        client = APIClient()
-        response = client.post('/api/save-employees', data, format='json')
+        response = self.client.post(reverse('save-employeesview'), data, format='json')
 
-        assert response.status_code == 400
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
