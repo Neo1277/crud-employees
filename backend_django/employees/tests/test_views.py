4d94a06a9aba29764e30001166a53555a76cfe8d
Marlon Meneses <marlon-armando@hotmail.com>
2022-08-08 09:41:42 -0500

    Update view tests added

--- backend_django/employees/tests/test_views.py
+++ backend_django/employees/tests/test_views.py
@@ -8,11 +8,16 @@ from employees.models import ThirdParties, Employees
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
 
-
+"""
+Correct way to make tests:
+https://stackoverflow.com/q/60892321
+"""
 class RetrieveEmployeesViewTest(APITestCase):
 
     @classmethod
     def setUpTestData(cls):
+
+        cls.current_date = datetime.now()
 
         cls.first_area = Areas.objects.create(
             description="Administration"
@@ -37,7 +42,7 @@ class RetrieveEmployeesViewTest(APITestCase):
             third_party=cls.first_third_party,
             country='CO',
             area=cls.first_area,
-            date_of_entry='2022-08-06 00:00:00'
+            date_of_entry=cls.current_date
         )
 
     def test_get_endpoint(self):
@@ -92,11 +97,11 @@ class CreateEmployeesViewTest(RetrieveEmployeesViewTest):
             'second_surname': 'RAMIREZ',
             'first_name': 'JUAN',
             'middle_names': 'RUBEN',
-            'email': 'juan.montoya@cidenet.com.us',
+            'email': 'juan.montoya@cidenet.com.co',
             'types_of_identity_documents_id': self.first_types_of_identity_documents.pk,
             'third_parties_employees':
                 {
-                    'country': 'US',
+                    'country': 'CO',
                     'date_of_entry': current_date,
                     'area_id': self.first_area.pk
                 }
@@ -173,5 +178,161 @@ class CreateEmployeesViewTest(RetrieveEmployeesViewTest):
         }
 
         response = self.client.post(reverse('save-employeesview'), data, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+
+class UpdateEmployeesViewTest(RetrieveEmployeesViewTest):
+
+
+    @classmethod
+    def setUpTestData(cls):
+        super(UpdateEmployeesViewTest, cls).setUpTestData()
+
+        cls.second_update_third_party = ThirdParties.objects.create(
+            identity_document="999998874",
+            types_of_identity_documents=cls.first_types_of_identity_documents,
+            last_name="MONTOYA",
+            second_surname='PEREZ',
+            first_name='JUAN',
+            middle_names='BERNARDO DE LOS RIOS',
+            email='juan.montoya.1@cidenet.com.co'
+        )
+
+        cls.second_update_employee = Employees.objects.create(
+            third_party=cls.second_update_third_party,
+            country='CO',
+            area=cls.first_area,
+            date_of_entry=cls.current_date
+        )
+
+    def test_put_endpoint(self):
+
+        current_date = datetime.now()
+        data = {
+            'identity_document': '999998874',
+            'last_name': 'MONTOYA',
+            'second_surname': 'CASTILLO',
+            'first_name': 'JUAN',
+            'middle_names': 'BERNARDO DE LOS RIOS',
+            'email': 'juan.montoya.1@cidenet.com.co',
+            'types_of_identity_documents_id': self.first_types_of_identity_documents.pk,
+            'third_parties_employees':
+                {
+                    'country': 'CO',
+                    'date_of_entry': current_date,
+                    'area_id': self.first_area.pk
+                }
+        }
+
+        response = self.client.put(
+            reverse('update-employeesview', kwargs={'pk': self.second_update_third_party.pk}),
+            data, format='json'
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_put_endpoint_send_repeated_data(self):
+
+        current_date = datetime.now()
+        invalid_data = {
+            'identity_document': '99999887',
+            'last_name': 'MONTOYA',
+            'second_surname': 'RAMIREZ',
+            'first_name': 'JUAN',
+            'middle_names': 'RUBEN',
+            'email': 'juan.montoya@cidenet.com.co',
+            'types_of_identity_documents_id': self.first_types_of_identity_documents.pk,
+            'third_parties_employees':
+                {
+                    'country': 'CO',
+                    'date_of_entry': current_date,
+                    'area_id': self.first_area.pk
+                }
+        }
+
+        response = self.client.put(
+            reverse('update-employeesview', kwargs={'pk': self.second_update_third_party.pk}),
+            invalid_data, format='json'
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+
+    def test_put_endpoint_send_invalid_data(self):
+
+        current_date = datetime.now()
+        invalid_data = {
+            'identity_document': '999998849777',
+            'last_name': 'cárdona',
+            'second_surname': 'RAMIREZ',
+            'first_name': 'Luis',
+            'middle_names': 'RUBEN',
+            'email': 'luis.cardona@cidenet.com.us',
+            'types_of_identity_documents_id': self.first_types_of_identity_documents.pk,
+            'third_parties_employees':
+                {
+                    'country': 'US',
+                    'date_of_entry': current_date,
+                    'area_id': self.first_area.pk
+                }
+        }
+
+        response = self.client.put(
+            reverse('update-employeesview', kwargs={'pk': self.second_update_third_party.pk}),
+            invalid_data, format='json'
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_put_endpoint_date_after_valida_range(self):
+
+        date_after_valida_range = datetime.now() + relativedelta(days=1)
+        data = {
+            'identity_document': '999998874',
+            'last_name': 'MONTOYA',
+            'second_surname': 'CASTILLO',
+            'first_name': 'JUAN',
+            'middle_names': 'BERNARDO DE LOS RIOS',
+            'email': 'juan.montoya.1@cidenet.com.co',
+            'types_of_identity_documents_id': self.first_types_of_identity_documents.pk,
+            'third_parties_employees':
+                {
+                    'country': 'CO',
+                    'date_of_entry': date_after_valida_range,
+                    'area_id': self.first_area.pk
+                }
+        }
+
+        response = self.client.put(
+            reverse('update-employeesview', kwargs={'pk': self.second_update_third_party.pk}),
+            data, format='json'
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_put_endpoint_date_before_valida_range(self):
+
+        date_before_valida_range = datetime.now() - relativedelta(months=2)
+        data = {
+            'identity_document': '999998874',
+            'last_name': 'MONTOYA',
+            'second_surname': 'CASTILLO',
+            'first_name': 'JUAN',
+            'middle_names': 'BERNARDO DE LOS RIOS',
+            'email': 'juan.montoya.1@cidenet.com.co',
+            'types_of_identity_documents_id': self.first_types_of_identity_documents.pk,
+            'third_parties_employees':
+                {
+                    'country': 'CO',
+                    'date_of_entry': date_before_valida_range,
+                    'area_id': self.first_area.pk
+                }
+        }
+
+        response = self.client.put(
+            reverse('update-employeesview', kwargs={'pk': self.second_update_third_party.pk}),
+            data, format='json'
+        )
 
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
